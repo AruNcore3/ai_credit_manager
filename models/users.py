2b788de4383d1b874348c428bb84fa7734b75bbe
AruNcore3 <venomryukn@gmail.com>
2026-04-08 23:21:50 +0530

    creating a user class to create a user table

--- models/users.py
+++ models/users.py
@@ -1,1 +1,20 @@
+from sqlalchemy.orm.session import Session
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+class User(BaseModel):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String, nullable=False)
+
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+    wallet = relationship("Wallet", back_populates="user",uselist=False)
+    ledger = relationship("Ledger", back_populates="user")
+
+if __name__ == "__main__":
+    pass
