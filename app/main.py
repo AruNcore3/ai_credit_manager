db82ac4557e2931aa5e2470ed79fb086c71f698f
AruNcore3 <venomryukn@gmail.com>
2026-04-10 21:22:16 +0530

    saving stripe credentials at .env

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-import fastapi as FastAPI
+from fastapi import FastAPI
+from fastapi import Request
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello, World!"}
+
+@app.post('/webhook')
+def webhook(request: Request):
+    return {"message": "Webhook received"}
