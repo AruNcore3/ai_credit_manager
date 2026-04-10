64e8ab2d25c7ae1821e4a862a68ef55ed046d525
AruNcore3 <venomryukn@gmail.com>
2026-04-10 18:15:02 +0530

    just did a minor changes

--- models/users.py
+++ models/users.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-class User(BaseModel):
+class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
